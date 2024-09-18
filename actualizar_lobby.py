80d9eedd2cc9470967992f9a46d18a037b596f2b
MatiasScantamburlo <matias.scantamburlo@mi.unc.edu.ar>
2024-09-18 14:05:45 -0300

    test: Se agrega test para encapsulamiento de mensajeria en lobbies
    - Implementado test_broadcast para verificar que los mensajes enviados a un
        lobby no se envíen a los jugadores en otros lobbies.
    - Se implementa un manejador de timeout receive_text_with_timeout para
        gestionar el tiempo de espera en la recepción de mensajes.
    
     Cambios a ser confirmados:
    	modificados:     actualizar_lobby.py

--- actualizar_lobby.py
+++ actualizar_lobby.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.testclient import TestClient
 from typing import List, Dict
 
+import signal
+
 app = FastAPI()
 
 
@@ -49,10 +51,10 @@ async def websocket_endpoint(websocket: WebSocket, lobby_id: int):
 
         await manejador.broadcast(f"Un jugador ha abandonado el lobby {lobby_id}.", lobby_id)
 
-# ------ TEST ------ #
-
+# ------ TEST 1 ------ #
 # Los jugadores reciben notificaciones cuando alguien se conecta o se
 # desconecta.
+
 def test_conectar_y_desconectar():
     with TestClient(app) as client:
         with client.websocket_connect("/ws/lobby/1") as clientws1:
@@ -69,3 +71,35 @@ def test_conectar_y_desconectar():
                 clientws1.close()
                 data3 = clientws2.receive_text()
                 assert data3 == "Un jugador ha abandonado el lobby 1."
+
+# ------ TEST 2 ------ #
+# Los mensajes de un lobby no se envían a los jugadores de otro lobby.
+
+# Manejador de timeout
+def handler(signum, frame):
+    raise TimeoutError("El tiempo de espera ha sido excedido")
+
+def receive_text_with_timeout(ws, timeout=2):
+    # Configurar la señal para que lance el manejador después del timeout
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(timeout)
+    
+    try:
+        data = ws.receive_text()
+        signal.alarm(0)  # Desactivar la alarma si se completa antes del timeout
+        return data
+    except TimeoutError as e:
+        print(e)
+        return None
+
+def test_broadcast():
+    client = TestClient(app)
+
+    with client.websocket_connect("/ws/lobby/1") as clientws1:
+        # Ignorar el primer mensaje de conexión
+        _ = clientws1.receive_text()
+
+        with client.websocket_connect("/ws/lobby/2") as clientws2:
+            
+            data = receive_text_with_timeout(clientws1)
+            assert data is None
