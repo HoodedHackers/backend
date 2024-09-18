913491dbfcda7f91610f91093fcefc7f42b06940
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2024-09-18 20:59:45 -0300

    SCRUM-14 cleanup

--- test_sanity.py
+++ test_sanity.py
@@ -1,13 +1,12 @@
 from fastapi.testclient import TestClient
 import asserts
 
-from main import app, db
+from main import app
 
 client = TestClient(app)
 
 
 def test_borrame():
-    db.create_tables()
     response = client.get("/api/borrame")
     asserts.assert_equal(response.status_code, 200)
     asserts.assert_equal(response.json(), {"players": []})
