89099847fc56d998a9148c083cb5e67fd49cd55c
seedLou <lourdes_reyes.545@mi.unc.edu.ar>
2024-09-23 02:04:23 -0300

    solucionando errores, solo queda tipo de dato UUID

--- tests/test_set_name.py
+++ tests/test_set_name.py
@@ -1,8 +1,12 @@
-import requests
+from fastapi.testclient import TestClient
 import pytest 
 import asserts
 from database import Database
 from model import Player
+from main import app
+
+client = TestClient(app)
+
 db = Database() 
 Session = db.get_session()
 
@@ -15,7 +19,7 @@ def name_of_player():
 
 @pytest.mark.end2end_test
 def test_set_name_endpoint(name_of_player):
-    response = requests.post(f"http://0.0.0.0:8000/api/name", json={"name": "Alice"})
+    response = client.post("/api/name", json={"name": "Alice"})
     player = Session.query(Player).filter_by(identifier="01").first()
     assert player is not None
     assert response.status_code == 201
