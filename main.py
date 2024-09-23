ee342189440294e1684d39aabf34f9ccc621f041
seedLou <lourdes_reyes.545@mi.unc.edu.ar>
2024-09-23 16:27:05 -0300

    corrigiendo errores de endpoint unirse a partida: bd, session, ojo variable hola

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from os import getenv
-from fastapi import FastAPI, Response, Request, Depends
+from fastapi import FastAPI, Response, Request, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import Database
 from repositories import GameRepository, PlayerRepository
-from unirse_a_partida import req_in, unirse_partida
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
 db_uri = getenv("DB_URI")
 if db_uri is not None:
@@ -11,13 +12,17 @@ if db_uri is not None:
 else:
     db = Database()
 db.create_tables()
+hola = db.session()
 app = FastAPI()
-game_repo = GameRepository(db.session())
+
+game_repo = GameRepository(hola)
+player_repo = PlayerRepository(hola)
 
 
 @app.middleware("http")
 async def add_game_repo_to_request(request: Request, call_next):
     request.state.game_repo = game_repo
+    request.state.player_repo = player_repo 
     response = await call_next(request)
     return response
 
@@ -28,8 +33,11 @@ def get_games_repo(request: Request) -> GameRepository:
 def get_player_repo(request: Request) -> PlayerRepository:
     return request.state.player_repo
 
+class req_in(BaseModel):  
+    id_game: int
+    id_player: int
 
-@app.put("/api/lobby7{id_game}")
+@app.put("/api/lobby/{id_game}")
 async def endpoint_unirse_a_partida(
     req: req_in, 
     games_repo: GameRepository= Depends(get_games_repo), 
@@ -37,5 +45,12 @@ async def endpoint_unirse_a_partida(
     ):
     selec_player = player_repo.get(req.id_player)
     selec_game = games_repo.get(req.id_game)
-    return unirse_partida(req, selec_game, selec_player)
+    if selec_player is None or selec_game is None:
+        raise HTTPException(status_code=404, detail="xd")
+    selec_game.add_player(selec_player)
+    games_repo.save(selec_game)
+    return jsonable_encoder(selec_game)
+
+        
+    
     
---
+++ tests/test_unirse_partida.py
@@ -0,0 +1,47 @@
+import pytest
+from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, patch
+import asserts
+from main import app
+
+client = TestClient(app)
+
+@pytest.fixture
+def player_a():
+    return {
+        "id": 1,
+        "name": "Alice"
+    }
+
+@pytest.fixture
+def game_pos():
+    return{
+        "id": 1,
+        "name": "partida",
+        "max_players": 2,
+        "min_players": 2,
+        "started": False,
+        "players": [{"id": 0, "name": "Owner"}, {"id": 1, "name": "Alice"}]
+    }
+
+@pytest.fixture
+def game_pre():
+    return{
+        "id": 1,
+        "name": "partida",
+        "max_players": 2,
+        "min_players": 2,
+        "started": False,
+        "players": [{"id": 0, "name": "Owner"}]
+    }
+
+@patch("main.GameRepository")
+@patch("main.PlayerRepository")
+def test_post_new_player(mock_GameRepository, mock_PlayerRepository, game_pre, game_pos, player_a):
+    mock_GameRepository.get.return_value = game_pre
+    mock_PlayerRepository.get.return_value = player_a
+
+    response = client.post("/api/lobby/1", json={"id_game": 1, "id_player":1})
+
+    asserts.assert_equal(response.status_code, 200)
+    asserts.assert_equal(response.json, game_pos)
