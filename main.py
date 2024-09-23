216afcda141b1483cb6ff4d0a7aa206549b6f6cf
LourdesMamani00 <lourdes.mamani@mi.unc.edu.ar>
2024-09-22 22:59:22 -0300

    SWC-46: no funca pero es mejor subirlo, fallan los tests pero ando volada

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import Session
 
 from database import Database
 from repositories import GameRepository
+from pydantic import BaseModel
+from fastapi import HTTPException
+from typing import List
+import random
 
 db_uri = getenv("DB_URI")
 if db_uri is not None:
@@ -31,3 +35,45 @@ def get_games_repo(request: Request) -> GameRepository:
 async def borrame(games_repo: GameRepository = Depends(get_games_repo)):
     games = games_repo.get_many(10)
     return {"games": games}
+
+
+class PlayerOutRandom(BaseModel):
+    id: int
+    name: str
+
+
+class GamePlayerResponse(BaseModel):
+    game_id: int
+    players: List[PlayerOutRandom]
+
+
+@app.post("/api/start_game", response_model=GamePlayerResponse)
+async def sortear_jugadores(
+    game_id: int, game_repo: GameRepository = Depends(get_games_repo)
+):
+
+    game = game_repo.get(game_id)
+    if game is None:
+        raise HTTPException(status_code=404, detail="Partida no encontrada")
+
+    elif len(game.players) < game.min_players:
+        raise HTTPException(
+            status_code=412,
+            detail="No se puede sortear jugadores si no hay suficientes jugadores",
+        )
+    elif game.started == True:
+        random.shuffle(game.players)
+        game_repo.save(game)
+
+        players_out = [
+            PlayerOutRandom(id=player.id, name=player.name) for player in game.players
+        ]
+    else:
+        raise HTTPException(
+            status_code=412,
+            detail="No se puede sortear jugadores si la partida no ha comenzado",
+        )
+
+    return GamePlayerResponse(game_id=game_id, players=players_out)
+
+
--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,6 +1,11 @@
 from fastapi.testclient import TestClient
 
 import asserts
+from unittest.mock import MagicMock
+from main import get_games_repo
+from pydantic import BaseModel
+from model import Game, Player
+from unittest.mock import AsyncMock, patch
 
 from main import app
 
@@ -11,3 +16,91 @@ def test_borrame():
     response = client.get("/api/borrame")
     asserts.assert_equal(response.status_code, 200)
     asserts.assert_equal(response.json(), {"games": []})
+
+
+
+
+
+class PlayerOutRandom(BaseModel):
+    id: int
+    name: str
+
+
+@patch("main.GameRepository")
+@patch("random.shuffle")
+def test_sortear_jugadores(mock_game_repo, mock_shuffle):
+    game_id = 1
+    player1 = PlayerOutRandom(id=0, name="Player 1")
+    player2 = PlayerOutRandom(id=1, name="Player 2")
+    game = Game(
+        id=game_id,
+        name="ayiiiuuuudaaaa",
+        players=[player1, player2],
+        min_players=2,
+        max_players=4,
+        started=True,
+        host=True,
+    )
+
+    mock_game_repo.return_value.get.return_value = game
+    mock_game_repo.return_value.save = AsyncMock()
+
+    response = client.post(f"/api/start_game?game_id={game_id}")
+
+    print("que chuchaaaaaaaaaaaaaaaaaaaaaaaa")
+    print(response.json())
+
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["game_id"] == game_id
+    assert "players" in response_data
+    assert len(response_data["players"]) == len(game.players)
+
+    # Verifica que los jugadores han sido sorteados
+    assert sorted([p.name for p in game.players]) == sorted(
+        [p["name"] for p in response_data["players"]]
+    )
+    assert response_data["players"] != [
+        {"id": p.id, "name": p.name} for p in game.players
+    ]  # Verifica que el orden ha cambiado
+
+
+# Prueba que maneja el caso cuando no hay suficientes jugadores
+@patch("main.GameRepository")
+def test_sortear_jugadores_no_sufficient_players(mock_game_repo):
+    game_id = 1
+    player1 = PlayerOutRandom(id=0, name="Player 1")
+    game = Game(
+        id=game_id,
+        name="LaWea",
+        players=[player1],
+        min_players=2,
+        max_players=4,
+        started=True,
+        host=True,
+    )
+
+    mock_game_repo.return_value.get.return_value = game
+
+    response = client.post(f"/api/start_game?game_id={game_id}")
+
+    print(response.json())
+
+    assert response.status_code == 412
+    assert response.json() == {
+        "detail": "No se puede sortear jugadores si no hay suficientes jugadores"
+    }
+
+
+# Prueba que maneja el caso cuando no se encuentra el juego
+@patch("main.GameRepository")
+def test_sortear_jugadores_game_not_found(mock_game_repo):
+    mock_game_repo.return_value.get.return_value = None
+
+    response = client.post("/api/start_game", json={"game_id": 9999})
+    
+    print(response.json())
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Partida no encontrada"}
+
