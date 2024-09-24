25d3fb57e331460f31a56641323f062050d8c067
MatiasScantamburlo <matias.scantamburlo@mi.unc.edu.ar>
2024-09-24 00:15:26 -0300

    test: Se agrega test get_game_not_found
    - Se implementa test para verificar que se retorne correctamente un
        mensaje 404 "Lobby not found" en caso de no existir lobby con el id
        solicitado.
    
     Cambios a ser confirmados:
    	modificados:     tests/test_get_game.py

--- tests/test_get_game.py
+++ tests/test_get_game.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 import pytest
-from main import app  # Asegúrate de que la aplicación esté correctamente importada
+from main import app
 
-# Crear un cliente de pruebas
+
 client = TestClient(app)
 
-# Datos simulados para la respuesta del repositorio
+
 data_out = {
     "id": 1,
     "max_players": 4,
@@ -22,3 +22,10 @@ def test_get_game():
         response = client.get("/api/lobby/1")
         assert response.status_code == 200
         assert response.json() == data_out
+
+def test_get_game_not_found():
+    with patch("repositories.game.GameRepository.get") as mock_get:
+        mock_get.return_value = None
+        response = client.get("/api/lobby/1")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Lobby not found"}
