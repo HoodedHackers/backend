015746bfc949ad151d3591e50e5ce51d2ad4f49d
MatiasScantamburlo <matias.scantamburlo@mi.unc.edu.ar>
2024-09-27 12:27:59 -0300

    Actualiza data_out en los tests para usar una instancia de Game
    - Modificado data_out en los tests, pasando de un diccionario simple a una
        instancia de Game.
    - La nueva instancia de Game incluye una lista de jugadores, mejorando la
        precisión en las pruebas al reflejar la estructura actual de los modelos.
    
     Cambios a ser confirmados:
    	modificados:     tests/test_get_game.py

--- tests/test_get_game.py
+++ tests/test_get_game.py
@@ -1,27 +1,36 @@
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 import pytest
-from main import app
 
+from main import app
+from model import Game, Player
 
 client = TestClient(app)
 
 
-data_out = {
-    "id": 1,
-    "max_players": 4,
-    "started": False,
-    "name": "Game of Thrones",
-    "current_player_turn": 0,
-    "min_players": 2
-}
+data_out = Game(
+    id=1,
+    name="Game of Thrones",
+    current_player_turn=0,
+    max_players=4,
+    min_players=2,
+    started=False,
+    players=[Player(id=1, name="Player 1"), Player(id=2, name="Player 2")]
+)
 
 def test_get_game():
     with patch("repositories.game.GameRepository.get") as mock_get:
         mock_get.return_value = data_out
         response = client.get("/api/lobby/1")
         assert response.status_code == 200
-        assert response.json() == data_out
+        assert response.json() == {
+            "name": "Game of Thrones",
+            "current_players": 2,
+            "max_players": 4,
+            "min_players": 2,
+            "is_started": False,
+            "turn": 0
+        }
 
 def test_get_game_not_found():
     with patch("repositories.game.GameRepository.get") as mock_get:
