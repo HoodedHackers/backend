f663ebfa0c6efb010ba42e5b8c11b55a9c366bd5
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2024-09-29 14:25:27 -0300

    SWC-36 Ahora hay una forma de guardar tableros en la db con un string :)

---
+++ model/board.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from typing_extensions import List
+
+from sqlalchemy.types import VARCHAR, TypeDecorator
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    YELLOW = 4
+
+
+class Board(TypeDecorator):
+    impl = VARCHAR
+
+    def process_bind_param(self, value: List[Color] | None, dialect):
+        if value is None:
+            return None
+        return "".join(f"{c.value}" for c in value)
+
+    def process_result_value(self, value, dialect) -> List[Color]:
+        if value is None:
+            return []
+        return [Color(int(c)) for c in value]
