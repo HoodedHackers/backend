1e3a098bf614877cabcdd213f2896766ee362b08
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2024-10-26 17:40:37 -0300

    SWC-204 Repartido al finalizar turno

--- main.py
+++ main.py
@@ -447,6 +447,9 @@ async def advance_game_turn(
         raise HTTPException(status_code=401, detail="Game hasn't started yet")
     current_player = game.current_player()
     assert current_player is not None
+    cards = game.add_random_card(player.id)
+    manager = Managers.get_manager(ManagerTypes.CARDS_FIGURE)
+    await manager.broadcast({"player_id": player.id, "cards": cards}, game_id)
     turn_manager = Managers.get_manager(ManagerTypes.TURNS)
     await turn_manager.broadcast(
         {
